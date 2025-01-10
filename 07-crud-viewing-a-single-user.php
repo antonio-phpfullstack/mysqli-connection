d57903a06c15f86aa5809c72a4835895ea9e9ea5
Antonio Gutierrez <antonio@phpfullstack.com.br>
2025-01-10 18:26:10 -0300

    07-Added CRUD script for viewing a single user

---
+++ 07-crud-viewing-a-single-user.php
@@ -0,0 +1,37 @@
+<?php
+$conn = require '06-connection.php';
+$result = $conn->query("SELECT * FROM users");
+$users = $result->fetch_all(MYSQLI_ASSOC);
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRUD</title>
+</head>
+<body>
+<table>
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($users as $user):?>
+        <tr>
+            <td><?=$user["id"]?></td>
+            <td><?=$user["name"]?></td>
+            <td><?=$user["email"]?></td>
+            <td><a href="07-show.php?id=<?=$user["id"]?>">Show</a></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+
+</table>
+</body>
+</html>
